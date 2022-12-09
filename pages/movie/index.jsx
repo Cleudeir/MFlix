654c2e12a593534cdfb0b1d7ab92ff00068f8053
Cleudeir <Cleudeirsilva@gmail.com>
2022-12-09 12:48:44 -0300

    update

--- pages/movie/index.jsx
+++ pages/movie/index.jsx
@@ -9,7 +9,7 @@ export async function getStaticProps(context) {
       dataGenres,
       type,
     },
-    revalidate: 7 * 24 * 60 * 60,
+    revalidate: 2,
   };
 }
 const Movies = function Movies({ dataGenres, type }) {
